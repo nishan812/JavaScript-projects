800d928e71250fb4fd4de9e433fffd85bf488819
Nishan Bista <nishanbista525@gmail.com>
2024-02-07 12:52:58 +0545

    loading optimization

--- src/MainPage/loader.js
+++ src/MainPage/loader.js
@@ -3,14 +3,16 @@ const contBody = document.querySelector(".mainContainer")
 const loader = document.querySelector(".qwerty");
 const body=document.querySelector("body")
 
+
+
 header.style.display="none"
 contBody.style.display="none"
 body.style.backgroundColor="#000000"
-
 setTimeout(() => {
     header.style.display="initial"
     contBody.style.display="initial"
     loader.remove()
     body.style.backgroundColor="#F9EFDB"
+    
 
 }, 3100);
