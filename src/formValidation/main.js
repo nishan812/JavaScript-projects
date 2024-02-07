7304ad02cda90bb281e66019c6c287b00edbf6d4
Nishan Bista <nishanbista525@gmail.com>
2024-02-07 13:31:45 +0545

    got vars and stored from html to js

---
+++ src/formValidation/main.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+const firstName = document.querySelector("#firstname");
+const middleName = document.querySelector("#middlename");
+const lastName = document.querySelector("#lastname");
+const gender = document.querySelector("#gender");
+const age = document.querySelector("#age");
+const phone = document.querySelector("#phoneNumber");
+const email = document.querySelector("#emailAddress");
+
+//Permanent address
+const pProvince = document.querySelector("#permanentProvince");
+const pDistrict = document.querySelector("#permanentDistrict");
+const pMunicipality = document.querySelector("#permanentMunicipality");
+const pWard = document.querySelector("#permanentWard");
+
+//Current Address
+const cProvince = document.querySelector("#currentProvince");
+const cDistrict = document.querySelector("#currentDistrict");
+const cMunicipality = document.querySelector("#currentMunicipality");
+const cWard = document.querySelector("#currentWard");
+
+
