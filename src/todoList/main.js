1ee893727ea69581541f891e7e409798db4f0dfd
Nishan Bista <nishanbista525@gmail.com>
2024-02-15 01:17:01 +0545

    sucess delete button

--- src/todoList/main.js
+++ src/todoList/main.js
@@ -4,70 +4,87 @@ const root = document.querySelector(".root");
 // Creating a input body for user input
 const inputBody = document.createElement("div");
 inputBody.classList.add("inputBody");
-root.appendChild(inputBody)
+root.appendChild(inputBody);
 
 // inserting add box
 const input = document.createElement("input");
-input.classList.add("userInput")
-input.setAttribute("href","text")
-inputBody.appendChild(input)
+input.classList.add("userInput");
+input.setAttribute("href", "text");
+inputBody.appendChild(input);
 
-//Creating Add Task button  
+//Creating Add Task button
 const addTaskButton = document.createElement("button");
 addTaskButton.classList.add("addTaskButton");
-addTaskButton.textContent="Add Task"
-inputBody.appendChild(addTaskButton)
+addTaskButton.textContent = "Add Task";
+inputBody.appendChild(addTaskButton);
 
-// Creating body for tasks list items 
+// Creating body for tasks list items
 const listItemsBody = document.createElement("div");
-listItemsBody.classList.add("listItemBody")
-root.appendChild(listItemsBody)
-
+listItemsBody.classList.add("listItemBody");
+root.appendChild(listItemsBody);
 
 // Creating array for storing tasks list all items
-const tasks =[]
+const tasks = [];
 
-
-// Event after clicking on Add Task Button
-addTaskButton.addEventListener("click",()=>{
-
-// Conditon for styling : if array is empty no padding 
-if(!tasks){
-    listItemsBody.style.padding="0"
-}else{
-    listItemsBody.style.padding="2%"
+// A function that style padding for listed item, if called fixes the padding according to the condition.
+function stylePadding() {
+  // Conditon for styling : if array is empty no padding
+  if (!tasks) {
+    listItemsBody.style.padding = "0";
+  } else {
+    listItemsBody.style.padding = "2%";
+  }
 }
-console.log(!tasks);
 
-// Clearing the items listed first.
-listItemsBody.innerHTML=""
+if (!input) {
+  // Event after clicking on Add Task Button
+  addTaskButton.addEventListener("click", () => {
+    stylePadding();
 
-// getting userinput 
-const userInput = document.querySelector(".userInput")
-tasks.push(userInput.value)
+    // Clearing the items listed first.
+    listItemsBody.innerHTML = "";
 
+    // getting userinput
+    const userInput = document.querySelector(".userInput");
+    tasks.push(userInput.value);
 
-// Showing tasks added by user
-tasks.forEach((task)=>{
-    const userTasks = document.createElement("li");
-    userTasks.classList.add("taskItem")
-    userTasks.textContent=task
-    listItemsBody.appendChild(userTasks)
-})
+    // Showing tasks added by user
+    tasks.forEach((task) => {
+      // Creatring a li and adding the user added task value and display it.
+      const userTasks = document.createElement("li");
+      userTasks.classList.add("taskItem");
+      userTasks.textContent = task;
+      listItemsBody.appendChild(userTasks);
 
+      // Creating a  delete task button for added Items
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      //adding class for button
+      deleteButton.classList.add("deleteButton");
+      // Adding delete Button for each items that are added
+      userTasks.appendChild(deleteButton);
+    });
 
+    // Clearing textbox After adding tasks
+    input.value = "";
+  });
+}
 
-// Clearing textbox After adding tasks
-input.value=""
-}) 
+//Event for deleting the task items
+root.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.classList.contains("deleteButton")) {
+    const listItem = target.parentNode; // getting current list item
+    const itemText = listItem.firstChild.textContent;
+    listItem.remove();
+    console.log(`Before :${tasks}`);
+    console.log(itemText);
 
-
-
-
-
-
-
-
-
-
-
+    // Removing item from array
+    const itemToRemove = tasks.findIndex((task) => task === itemText);
+    if (itemToRemove !== -1) {
+      tasks.splice(itemToRemove, 1);
+      console.log(`After: ${tasks}`);
+    }
+  }
+});
