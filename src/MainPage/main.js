86d5b21635c3615da3ac5f38d66c94acf44ea421
Nishan Bista <nishanbista525@gmail.com>
2024-03-19 16:20:14 +0545

    code formating

--- src/MainPage/main.js
+++ src/MainPage/main.js
@@ -1,5 +1,4 @@
-const container = document.querySelector(".container")
-
+const container = document.querySelector(".container");
 
 // Button to redirect to BMI calculator
 const bmiRedirect = document.createElement("li");
@@ -14,8 +13,6 @@ const formValidationAnchor = document.createElement("a");
 formValidationRedirect.appendChild(formValidationAnchor);
 formValidationAnchor.innerHTML = "Form";
 formValidationAnchor.setAttribute("href", "/src/formValidation/index.html");
-
-
 
 // Button to redirect to Todo App
 const todoRedirect = document.createElement("li");
@@ -43,13 +40,10 @@ const PMG = document.createElement("li");
 const PMGAnchor = document.createElement("a");
 PMG.appendChild(PMGAnchor);
 PMGAnchor.innerHTML = "Popup Message Generation";
-PMGAnchor.setAttribute("href", "#");
-
-
+PMGAnchor.setAttribute("href", "");
 
 //gettinig root for box of list that contains projects that uses api list
 const apiProjectsBody = document.querySelector(".apiProjectsList");
-
 
 // Button that redirects to Quote Generator
 const quoteGeneratorRedirect = document.createElement("li");
@@ -63,7 +57,10 @@ const githubProfileViewerRedirect = document.createElement("li");
 const githubProfileViewerAnchor = document.createElement("a");
 githubProfileViewerRedirect.appendChild(githubProfileViewerAnchor);
 githubProfileViewerAnchor.innerHTML = "Github profile Viewer";
-githubProfileViewerAnchor.setAttribute("href", "/src/GithubProfileViewer/index.html");
+githubProfileViewerAnchor.setAttribute(
+  "href",
+  "/src/GithubProfileViewer/index.html"
+);
 
 // Button that redirects to Movie Recommendation System
 const mrsRedirect = document.createElement("li");
@@ -72,108 +69,105 @@ mrsRedirect.appendChild(mrsAnchor);
 mrsAnchor.innerHTML = "Movie Recommendation System";
 mrsAnchor.setAttribute("href", "#");
 
-
-
-
 // Button to redirect to Wikipedia Search System
 const wssRedirect = document.createElement("li");
 const wssAnchor = document.createElement("a");
-wssRedirect.appendChild(wssAnchor)
-wssAnchor.setAttribute("href","#");
-wssAnchor.innerHTML="Wikipedia Search"
+wssRedirect.appendChild(wssAnchor);
+wssAnchor.setAttribute("href", "#");
+wssAnchor.innerHTML = "Wikipedia Search";
 
 // Button to redirect to Dictionary App
 const dictAppRedirect = document.createElement("li");
 const dictAppAnchor = document.createElement("a");
-dictAppRedirect.appendChild(dictAppAnchor)
-dictAppAnchor.setAttribute("href","#");
-dictAppAnchor.innerHTML="Dictionary App"
+dictAppRedirect.appendChild(dictAppAnchor);
+dictAppAnchor.setAttribute("href", "#");
+dictAppAnchor.innerHTML = "Dictionary App";
 
 // Button to redirect to send Message on whatsapp
 const smowRedirect = document.createElement("li");
 const smowAnchor = document.createElement("a");
-smowRedirect.appendChild(smowAnchor)
-smowAnchor.setAttribute("href","#");
-smowAnchor.innerHTML="Send a Message to a User on Whatsapp"
+smowRedirect.appendChild(smowAnchor);
+smowAnchor.setAttribute("href", "#");
+smowAnchor.innerHTML = "Send a Message to a User on Whatsapp";
 
+const items = [
+  {
+    link: bmiRedirect,
+    discription:
+      "Body mass index calculator, calculate your BMI and improve your health.",
+  },
+  {
+    link: formValidationRedirect,
+    discription: "A simple form with validation.",
+  },
+  {
+    link: todoRedirect,
+    discription:
+      " Advance To-Do list for managing your tasks, with advance features. ",
+  },
+  {
+    link: hcpRedirect,
+    discription: "Change backgroud according way you like. ",
+  },
+  {
+    link: digitalClockRedirect,
+    discription: "A simple digital clock.",
+  },
+  {
+    link: PMG,
+    discription:
+      "A popup message generator, Generates message after you click.",
+  },
+  {
+    link: quoteGeneratorRedirect,
+    discription: "A simple quote generator ",
+  },
+  {
+    link: githubProfileViewerRedirect,
+    discription: "Search any users on github and view their all details.",
+  },
+  {
+    link: mrsRedirect,
+    discription: "A system that suggests you movie that you may like. ",
+  },
+  {
+    link: smowRedirect,
+    discription: "Send message to user on whatsapp.",
+  },
+  {
+    link: wssRedirect,
+    discription: "Search on wikipedia made easy.",
+  },
+  {
+    link: dictAppRedirect,
+    discription: "A dictionary app for your english betterment.",
+  },
+];
 
-const items = [{
-  link:bmiRedirect,
-  discription:"Body mass index calculator, calculate your BMI and improve your health."
-},
-{
-link:formValidationRedirect,
-discription:"A simple form with validation."
-},
-{
-  link:todoRedirect ,
-  discription:" Advance To-Do list for managing your tasks, with advance features. "
-},
-{
-  link:hcpRedirect,
-  discription:"Change backgroud according way you like. "
-},
-{
-  link:digitalClockRedirect ,
-  discription:"A simple digital clock."
-},
-{
-  link:PMG ,
-  discription:"A popup message generator, Generates message after you click."
-}
-,
-{
-  link:quoteGeneratorRedirect ,
-  discription:"A simple quote generator "
-},
-{
-  link:githubProfileViewerRedirect ,
-  discription:"Search any users on github and view their all details."
-},
-{
-  link:mrsRedirect,
-  discription:"A system that suggests you movie that you may like. "
-},
-{
-  link:smowRedirect ,
-  discription:"Send message to user on whatsapp."
-},
-{
-  link:wssRedirect ,
-  discription:"Search on wikipedia made easy."
-},
-{
-  link:dictAppRedirect ,
-  discription:"A dictionary app for your english betterment."
-}
-]
-
-
-
-items.forEach((item)=>{
+items.forEach((item) => {
   const cardBody = document.createElement("div");
-  cardBody.classList.add("card")
+  cardBody.classList.add("card");
   const description = document.createElement("p");
-  description.classList.add("description")
-  description.textContent=item.discription
-  description.style.display="none"
+  description.classList.add("description");
+  description.textContent = item.discription;
+  description.style.display = "none";
 
-  cardBody.appendChild(item.link)
-  cardBody.appendChild(description)
-  container.appendChild(cardBody)
+  cardBody.appendChild(item.link);
+  cardBody.appendChild(description);
+  container.appendChild(cardBody);
 
-  cardBody.addEventListener("mouseover",()=>{
-    description.style.display="block"
-    description.style.fontSize="16px"
-  })
-  cardBody.addEventListener("mouseout",()=>{
-    description.style.display="none"
-    description.style.fontSize="10px"
-  })
+  cardBody.addEventListener("mouseover", () => {
+    description.style.display = "block";
+    description.style.fontSize = "16px";
+  });
+  cardBody.addEventListener("mouseout", () => {
+    description.style.display = "none";
+    description.style.fontSize = "10px";
+  });
 
-  cardBody.addEventListener("click",(e)=>{
-    const target = e.currentTarget
+  cardBody.addEventListener("click", (e) => {
+    const target = e.currentTarget;
     const linkHref = target.querySelector("a").getAttribute("href");
     window.location.href = linkHref;
-  })
-})
+  });
+});
