2f91d87c125240fdb76f8dd740e3980fb3b82651
Nishan Bista <nishanbista525@gmail.com>
2024-03-20 19:15:22 +0545

    api fetched sucessfully

--- src/GithubProfileViewer/main.js
+++ src/GithubProfileViewer/main.js
@@ -9,11 +9,36 @@ const searchBody = document.createElement("div");
 searchBody.classList.add("searchBody");
 root.appendChild(searchBody);
 
+//function to give alert messages
 function alertMessage(mes) {
-  alertBar.classList.remove("alertBar_h")
+  alertBar.classList.remove("alertBar_h");
   alertBar.textContent = `Alert: ${mes}`;
   setTimeout(() => {
-    alertBar.classList.add("alertBar_h")
+    alertBar.classList.add("alertBar_h");
   }, 3000);
 }
 
+const inputSearchBox = document.createElement("input");
+inputSearchBox.classList.add("inputSearchBox");
+inputSearchBox.setAttribute("placeholder", "1");
+searchBody.appendChild(inputSearchBox);
+
+const searchButton = document.createElement("button");
+searchButton.classList.add("searchButton");
+searchButton.textContent = "Search";
+searchBody.appendChild(searchButton);
+
+searchButton.addEventListener("click", () => {
+  if (inputSearchBox.value.trim() == "") {
+    alertMessage("username is empty");
+  } else {
+    inputSearchBox.value = "";
+    fetch(`https://api.github.com/users/${inputSearchBox.value}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }
+});
